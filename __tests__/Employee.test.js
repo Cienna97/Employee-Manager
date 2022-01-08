ca387a8da7b4e140ac61426dc58f89bc810a36ea
Cienna97 <cmortenson97@gmail.com>
2022-01-08 01:34:11 -0700

    adding to get name and id

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,18 +1,26 @@
-//name 
-const { TestWatcher } = require("jest");
-const employee = require("../lib/employee");
 
+//const { TestWatcher } = require("jest");
+
+const employee = require("../lib/employee");
+//name 
 test("Can instantiate employee instance", () => {
     const e = new employee();
     expect(typeof(e)).toBe("object");
 });
-
-
+//getName()
+test("Can set name via constructor arguments", () => {
+    const name = "Cienna";
+    const e = new employee(name);
+    expect(e.name).toBe(name);
+});
 //id
-
+test("Can set id via constructor argument", () => {
+    const testValue = 100;
+    const e = new employee("Foo", testValue);
+    expect(e.id).toBe(testValue);
+});
 //email
 
-//getName()
 
 //getId()
 
