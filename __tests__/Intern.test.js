1cd02b2240864e27dbb347b52117f42bf69ba87d
Cienna97 <cmortenson97@gmail.com>
2022-01-08 02:15:41 -0700

    adding test for intern school

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,19 +1,27 @@
 //name 
 const intern = require("../lib/intern");
-
+//school test value 
 test("Can set school via constructor", () => {
   const testValue = "UO";
   const e = new intern("Foo", 1, "test@test.com", testValue);
   expect(e.school).toBe(testValue);
 });
-//getId()
+//return intern
+test("getRole() should return \"intern\"", () => {
+  const testValue = "intern";
+  const e = new intern("Foo", 1, "test@test.com", "UO");
+  expect(e.getRole()).toBe(testValue);
+});
 
-//getEmail()
 
 //school
 
 //getSchool()
-
+test("Can get school via getSchool()", () => {
+  const testValue = "UO";
+  const e = new intern("Foo", 1, "test@test.com", testValue);
+  expect(e.getSchool()).toBe(testValue);
+});
 //getRole() Overridden to return 'Intern'
 
 
