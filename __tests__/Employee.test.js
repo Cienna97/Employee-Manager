e690a7fc5e005fb776f50661f6dbac2ec1e9b391
Cienna97 <cmortenson97@gmail.com>
2022-01-08 01:27:52 -0700

    working on employee test file

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,4 +1,12 @@
 //name 
+const { TestWatcher } = require("jest");
+const employee = require("../lib/employee");
+
+test("Can instantiate employee instance", () => {
+    const e = new employee();
+    expect(typeof(e)).toBe("object");
+});
+
 
 //id
 
