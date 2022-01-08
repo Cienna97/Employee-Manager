07eb21822511865bd73c1f3b018527f8782c5c6e
Cienna97 <cmortenson97@gmail.com>
2022-01-08 02:34:53 -0700

    return engineer

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,14 +1,20 @@
 //name 
 //const { TestWatcher } = require("jest");
 const engineer = require("../lib/engineer");
-
+//github Github username
 test("Can set GitHub account via constructor", () => {
     const testValue = "GitHubUser";
     const e = new engineer("Foo", 1, "test@test.com",
     testValue);
     expect(e.github).toBe(testValue);
 });
-//github Github username
+
+test("getRole() should return \"engineer\"", () => {
+    const testValue = "engineer";
+    const e = new engineer("Foo", 1, "test@test.com",
+    "GitHubUser");
+    expect(e.getRole()).toBe(testValue);
+});
 
 //getGithub()
 test("Can get GitHub username via getGithub()", () => {
