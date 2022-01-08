07bfc192bdd8c4c37b27fe667f3298d5191b7cdb
Cienna97 <cmortenson97@gmail.com>
2022-01-08 02:30:36 -0700

    engineer.test file set github

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,7 +2,12 @@
 //const { TestWatcher } = require("jest");
 const engineer = require("../lib/engineer");
 
-
+test("Can set GitHub account via constructor", () => {
+    const testValue = "GitHubUser";
+    const e = new engineer("Foo", 1, "test@test.com",
+    testValue);
+    expect(e.github).toBe(testValue);
+});
 //github Github username
 
 //getGithub()
