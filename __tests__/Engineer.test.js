04ea1efc93367d2019e948e7720127580a58b392
Cienna97 <cmortenson97@gmail.com>
2022-01-08 02:25:48 -0700

    test to get github user'

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,17 +1,15 @@
 //name 
+//const { TestWatcher } = require("jest");
+const engineer = require("../lib/engineer");
 
-//id
-
-//email
-
-//getName()
-
-//getId()
-
-//getEmail()
 
 //github Github username
 
 //getGithub()
-
+test("Can get GitHub username via getGithub()", () => {
+    const testValue = "GitHubUser";
+    const e = new engineer("Foo", 1, "test@test.com",
+    testValue);
+    expect(e.getGithub()).toBe(testValue);
+});
 //getRole() Overridden to return 'Engineer'
