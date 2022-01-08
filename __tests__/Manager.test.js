ce8fa6026ae4bde9fd5e2cd3d8b0153cba77a9fa
Cienna97 <cmortenson97@gmail.com>
2022-01-07 22:22:25 -0700

    creating manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,8 +1,14 @@
 //name 
-const Manager = require("../lib/Manager");
-const Employee = require("../lib/E")
+const Manager = require("../lib/manager");
+const Employee = require("../lib/employee");
+const { TestWatcher } = require("jest");
 //id
-
+test("getRole() should return \"Manager\"", () => {
+    const testValue = "Manager";
+    const e = new Manager("Foo", 1, "test@test.com",
+    testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
 //email
 
 //getName()
