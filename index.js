f0f88066321a7324296e0a30cd49ed6874dbdbd2
Cienna97 <cmortenson97@gmail.com>
2022-01-08 17:41:50 -0700

    writing arrays for manager name and id

--- index.js
+++ index.js
@@ -9,3 +9,39 @@ const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+
+const teamMembers = [];
+const idArray = [];
+
+function appMenu() {
+
+    function createManager() {
+      console.log("Please build your team");
+      inquirer.prompt([
+        {
+          type: "input",
+          name: "managerName",
+          message: "What is the manager's name?",
+          validate: answer => {
+            if (answer !== "") {
+              return true;
+            }
+            return "Enter a character";
+          }
+        },
+        {
+          type: "input",
+          name: "managerId",
+          message: "What is the manager's id?",
+          validate: answer => {
+            const pass = answer.match(
+              /^[1-9]\d*$/
+            );
+            if (pass) {
+              return true;
+            }
+            return "Enter a number greater than zero";
+          }
+        },
+        {
+  
